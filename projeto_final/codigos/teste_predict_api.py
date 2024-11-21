db145d3c041b90b7b10819ce66240709dd11c42c
Sanoj VM <jonasvm@users.noreply.github.com>
2024-11-21 14:53:18 -0300

    Update teste_predict_api.py

--- projeto_final/codigos/teste_predict_api.py
+++ projeto_final/codigos/teste_predict_api.py
@@ -3,23 +3,17 @@ from flask_cors import CORS
 import joblib
 import pandas as pd
 
-# Carregar o modelo
 model = joblib.load("diabetes_rf_model.pkl")
 
-# Criar a aplicação Flask
 app = Flask(__name__)
 
-# Ativar o CORS para a aplicação inteira
 CORS(app)
 
-# Rota para fazer a predição
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Receber os dados JSON da requisição
         data = request.get_json()
 
-        # Organizar os dados para o formato necessário pelo modelo
         input_data = {
             'Age': [data['Age']],
             'Gender': [data['Gender']],
@@ -39,17 +33,13 @@ def predict():
             'Obesity': [data['Obesity']]
         }
 
-        # Criar DataFrame com os dados recebidos
         input_df = pd.DataFrame(input_data)
 
-        # Fazer a previsão
         prediction = model.predict(input_df)
 
-        # Retornar o resultado da predição como resposta JSON
         return jsonify({'prediction': prediction[0]})
 
     except Exception as e:
-        # Caso ocorra algum erro, retornar uma mensagem de erro
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
